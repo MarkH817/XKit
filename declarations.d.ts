44be0794ee399ab4341e9fb9035e97f2c1f59a0f
Mark Hernandez <mark@lion-byte.com>
2018-10-13 13:11:16 -0500

    Start of experimental bundle

---
+++ declarations.d.ts
@@ -0,0 +1,12 @@
+interface ExtensionInfo {
+  id: string
+  version: string
+  title: string
+  developer: string
+  description: string
+  details: string
+
+  frame: boolean
+  beta?: boolean
+  slow?: boolean
+}
